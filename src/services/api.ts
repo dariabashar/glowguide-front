159c7a1d6566b40fd24c45744ffdf1c3337072d0
Darya Bashar <d.bashar@example.com>
2025-07-04 16:57:15 +0500

    Updates

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Настройка axios-инстанса
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
 // Авторизация — получение access_token
